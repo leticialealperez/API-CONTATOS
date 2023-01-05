05af5e879a8b796b3d50c212ec5d564119b40e39
Leticia Leal <leticia.leal@growdev.com.br>
2023-01-05 07:40:57 -0300

    criado metodo post e get de recados

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,30 @@
 import express, { Request, Response } from 'express';
 import 'dotenv/config';
+import { Contact, ResponseAPI } from './types';
+import { v4 as uuid } from 'uuid';
 
 const app = express();
 const port = process.env.PORT;
-app.use(express.json());
-
 app.listen(port, () => {
     console.log(`App rodando na porta ${port}`);
 });
 
-const listaNumeros: number[] = []
+app.use(express.json());
+
+const listaContatos: Contact[] = []
+
+// id, name, phone
+
+//   ?nome='Joao' - query params - caso de uso - filtragem de dados
+//   /numeros/:identificador - params - caso de uso salvar um registro para um usuario específico
 
 // GET - buscar - listagem - READ
-app.get('/numeros', (request: Request, response: Response) => {
-    const dobro = listaNumeros.map((v) => v * 2)
-
-
-    const resposta = {
-      mensagem: 'Deu bom',
-      numeros: dobro,
+app.get('/contatos', (request: Request, response: Response) => {
+    
+    const resposta: ResponseAPI = {
+      success: true,
+      message: 'Contatos buscados com sucesso',
+      data: listaContatos,
     };
 
     return response.status(200).send(resposta);
@@ -26,25 +32,76 @@ app.get('/numeros', (request: Request, response: Response) => {
 
 
 // POST - criar - CREATE
-app.post('/numeros', (request: Request, response: Response) => {
-    const { numero } = request.body // { numero: 80 }
+app.post('/contatos', (request: Request, response: Response) => {
+  const { name, phone } = request.body; // { "numero": 80 }
 
-    if(!numero) {
-        return response.status(400).send({
-          mensagem: 'Deu ruim, a propriedade número é obrigatória',
-        });
+  if (!name || !phone) {
+    // bad request - 400 - requisição inválida
+    const resposta: ResponseAPI = {
+        success: false,
+        message: 'As propriedades "name" e "phone" são obrigatórias.',
+        data: null
+    }
+    return response.status(400).send(resposta);
+  }
+
+    // remove caracteres especiais do telefone enviado pelo client
+    const phoneParsed = phone
+        .normalize('NFD')
+        .replace(/([\u0300-\u036f]|[^0-9])/g, '');
+
+    if(phoneParsed.length < 11) {
+        const resposta: ResponseAPI = {
+          success: false,
+          message: 'O telefone é inválido. Por favor, informe o DDD e o telefone corretamente.',
+          data: null,
+        };
+        return response.status(400).send(resposta);
     }
 
-    listaNumeros.push(numero);
+    const exist = listaContatos.some((contact) => contact.phone === phoneParsed);
 
-    return response.status(201).send({
-        mensagem: 'Deu bom, numero adicionado com sucesso!'
-    })
+    if(exist) {
+        const resposta: ResponseAPI = {
+          success: false,
+          message: 'Este telefone já foi cadastrado anteriormente. Verique sua lista de contatos!',
+          data: null,
+        };
+        return response.status(400).send(resposta);
+    }
 
+    const novoContato: Contact = {
+      id: uuid(),
+      name,
+      phone: phoneParsed,
+    };
+
+    listaContatos.push(novoContato);
+
+    const resposta: ResponseAPI = {
+      success: true,
+      message: 'Contato adicionado com sucesso!',
+      data: novoContato,
+    };
+
+    // SUCCESS - CREATED - 201
+    return response.status(201).send(resposta);
 });
 
 
 // PUT - atualizar - UPDATE
+app.put('/contatos/:identificador', (request: Request, response: Response) => {
+    const { identificador } = request.params;
+    const { teste } = request.body;
+
+    //...
+    // verificar se existe o registro pelo id
+    // se existir modificar o registro na lista e retornar o objeto atualizado
+    // usuario deve poder atualizar uma ou todas as propriedades do contato
+
+    return response.send(identificador);
+});
 
 
 // DELETE - deletar - DELETE
+app.delete('', () => {})
